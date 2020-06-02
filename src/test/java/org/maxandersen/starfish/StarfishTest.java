0fe582586130a6dfa765f1af38a2936f9390d545
fahad-israr <fahad00cms@gmail.com>
2020-06-02 14:05:10 +0530

    Addding test for URL Validator

---
+++ src/test/java/org/maxandersen/starfish/StarfishTest.java
@@ -0,0 +1,25 @@
+package org.maxandersen.starfish;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static org.maxandersen.starfish.Starfish.validate_url;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class StarfishTest {
+
+    @Test
+    public void testURLValidator() {
+        
+        assertEquals(true,validate_url("https://github.com/user-name/repo-name.git"),"URL Validation test failes");
+        assertEquals(true,validate_url("http://github.com/user-name/repo-name.git"),"URL Validation test failes");
+        assertEquals(true,validate_url("git@github.com:user-name/repo-name.git"),"URL Validation test failes");
+
+        
+    }
+
+}
